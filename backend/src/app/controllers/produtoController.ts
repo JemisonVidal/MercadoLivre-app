83536e503b62d98133f583c80d1f069e9722a6f0
Jemison Vidal <jemison.santoos@gmail.com>
2020-04-23 12:32:43 -0300

    criado controllers

--- backend/src/app/controllers/produtoController.ts
+++ backend/src/app/controllers/produtoController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import mercadoLivre from '../../utils/mercadoLivre';
 require('dotenv').config();
 
 class produtoController {
@@ -8,14 +9,59 @@ class produtoController {
     this.SYS_PWD = process.env.SYS_PWD;
   }
 
-  public show(req: Request, res: Response) {
+  public async show(req: Request, res: Response) {
     try {
+      const mercadoLivreObj = new mercadoLivre(res.locals.token);
+      const user = await mercadoLivreObj.get('/users/me');
+      const categories = await mercadoLivreObj.get(
+        `/sites/${user.site_id}/categories`
+      );
+      const currencies = await mercadoLivreObj.get('/currencies');
+      const listing_types = await mercadoLivreObj.get(
+        `/sites/${user.site_id}/listing_types`
+      );
+
+      res.status(200).json({ user, categories, currencies, listing_types });
     } catch (error) {
       res.status(500).send(`error:${error}`);
     }
   }
 
-  public create(req: Request, res: Response) {}
+  public async create(req: Request, res: Response) {
+    try {
+      const mercadoLivreObj = new mercadoLivre(res.locals.token);
+      const user = await mercadoLivreObj.get('/users/me');
+      const predict = await mercadoLivreObj.get(
+        `/sites/${
+          user.site_id
+        }/category_predictor/predict?title=${encodeURIComponent(
+          req.body.title
+        )}`
+      );
+      await mercadoLivreObj.post('/items', {
+        title: req.body.title,
+        category_id: predict.id,
+        price: req.body.price,
+        currency_id: req.body.currency,
+        available_quantity: req.body.quantity,
+        buying_mode: 'buy_it_now',
+        listing_type_id: req.body.listing_type,
+        condition: req.body.condition,
+        description: req.body.description,
+        tags: ['immediate_payment'],
+        pictures: [
+          {
+            source: `${req.protocol}://${req.get('host')}/pictures/${
+              req.body.file.filename
+            }`,
+          },
+        ],
+      });
+      res.status(200).json({ message: 'item created' });
+    } catch (error) {
+      res.status(500).send(`error:${error}`);
+    }
+  }
 }
 
 export default new produtoController();
--- backend/src/utils/mercadoLivre.ts
+++ backend/src/utils/mercadoLivre.ts
@@ -1,13 +1,26 @@
-import meli from 'mercadolibre';
+import mercadoLivre from 'mercadolibre';
 require('dotenv').config();
 
 class MercadoLivre {
-  CLIENT_ID: string;
-  CLIENT_SECRET: string;
-  constructor() {
-    this.CLIENT_ID = process.env.CLIENT_ID;
-    this.CLIENT_SECRET = process.env.CLIENT_SECRET;
+  token: string;
+  mercadoLivreObj: mercadoLivre.meli;
+
+  constructor(token: string) {
+    this.token = token;
+    this.mercadoLivreObj = new mercadoLivre.meli(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      token
+    );
+  }
+
+  private async geti(url: string, params?: string) {
+    return await this.mercadoLivreObj.get(url, params);
+  }
+
+  private async post(url: string, body: any, params?: string) {
+    return await this.mercadoLivreObj.post(url, body, params);
   }
 }
 
-export default new MercadoLivre();
+export default new MercadoLivre(this.token).mercadoLivreObj;
